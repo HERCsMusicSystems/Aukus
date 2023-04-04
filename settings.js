d180f9792eeab87f523380bd4d26f49a41552845
Robert Wolf <robert.wolf@hercsmusicsystes.com>
2023-04-04 14:16:28 +1000

    Effects On mechanics added.

--- settings.js
+++ settings.js
@@ -11,3 +11,6 @@ if (! side) localStorage . setItem ('side', side = sides [0]);
 
 var music_on = localStorage . getItem ('music_on');
 if (music_on === null) localStorage . setItem ('music_on', music_on = true); else music_on = music_on === 'true';
+
+var effects_on = localStorage . getItem ('effects_on');
+if (effects_on === null) localStorage . setItem ('effects_on', effects_on = true); else effects_on = effects_on === 'true';
