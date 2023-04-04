03273800404e790182a504f443bc594d65f57904
Robert Wolf <robert.wolf@hercsmusicsystes.com>
2023-04-04 12:34:52 +1000

    Side selector and music on checkbox added.

---
+++ settings.js
@@ -0,0 +1,13 @@
+var ranks = {
+	'USA': ['Ensign', 'Lieutenant', 'Lieutenant Commander', 'Commander', 'Captain'],
+	'United Kingdom': ['Sub-lieutenant', 'Lieutenant', 'Lieutenant Commander', 'Commander', 'Captain', 'Commodore'],
+	Russia: ['Лейтенант', 'Старший лейтенант', 'Капитан-лейтенант', 'Капитан третьего ранга', 'Капитан второго ранга', 'Капитан']
+};
+
+var sides = Object . keys (ranks);
+
+var side = localStorage . getItem ('side');
+if (! side) localStorage . setItem ('side', side = sides [0]);
+
+var music_on = localStorage . getItem ('music_on');
+if (music_on === null) localStorage . setItem ('music_on', music_on = true); else music_on = music_on === 'true';
