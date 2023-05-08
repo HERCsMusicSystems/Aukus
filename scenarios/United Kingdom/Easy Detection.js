ef6bf62d729d25bdbe0e211bff9ce6e4c75c0cb8
Robert <robert.wolf@hercsmusicsystems.com.au>
2023-05-08 19:42:50 +0930

    Easy Detection scenario added....

---
+++ scenarios/United Kingdom/Easy Detection.js
@@ -0,0 +1,5 @@
+
+MissionBackground = 'Iran is violating international agreements and keeps selling oil to troublesome countries.';
+MissionObjective = 'Detect the oiler and do not get detected yourself.';
+MissionConstructor = Virginia;
+
